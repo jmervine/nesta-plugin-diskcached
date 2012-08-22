c2e977e3c46828697ab6b5fc7319b83b9b40700d
Joshua Mervine <joshua@mervine.net>
2012-08-22 17:39:05 -0400

    adding Nesta::Plugin::Diskcached to init for compatability

--- lib/nesta-plugin-diskcached/init.rb
+++ lib/nesta-plugin-diskcached/init.rb
@@ -1,6 +1,17 @@
 # Top level Nesta module.
 module Nesta
 
+  # Nesta's Plugin module.
+  module Plugin
+
+    # Diskcached Plugin, which doesn't really exists
+    # as everything is done as overrides. I'm keeping
+    # it for Nesta's plugin interface compatability.
+    module Diskcached
+    end
+
+  end
+
   # Nesta's existing Overrides for rendering.
   module Overrides
 
